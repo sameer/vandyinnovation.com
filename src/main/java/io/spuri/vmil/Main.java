4cf8dd05807dea3e1a8a8829db73aa42d2a4d9db
Sameer Puri <spuri4096@gmail.com>
2017-09-30 18:04:39 -0500

    Add SSL handling and props using env vars

--- src/main/java/io/spuri/vmil/Main.java
+++ src/main/java/io/spuri/vmil/Main.java
@@ -1,12 +1,16 @@
 package io.spuri.vmil;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.net.PemKeyCertOptions;
 import io.vertx.ext.web.Router;
-import static io.spuri.vmil.VRoutes.*;
+
+import static io.spuri.vmil.VRoutes.dynamicroutes;
 
 public class Main extends AbstractVerticle {
   protected Router router;
   protected VTemplating vTemplating;
+
   @Override
   public void start() throws Exception {
     vTemplating = new VTemplating(vertx);
@@ -18,6 +22,8 @@ public class Main extends AbstractVerticle {
     for (RouteMaker rm : VRoutes.addstaticroutes(this)) {
       rm.make(this);
     }
-    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+    vertx.createHttpServer(
+      new HttpServerOptions().setSsl(true).setPemKeyCertOptions(new PemKeyCertOptions().addKeyPath(System.getProperty("vertx.key")).addCertPath(System.getProperty("vertx.cert"))))
+      .requestHandler(router::accept).listen(Integer.parseInt(System.getProperty("vertx.port")), System.getProperty("vertx.host"));
   }
 }
